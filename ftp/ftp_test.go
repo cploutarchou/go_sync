0e412a9dd4cf54489a1f9a645f2d5ea14ab1e8eb
Christos Ploutarcou <cploutarchou@gmail.com>
2023-07-17 18:59:16 +0300

    implement test for watching directory functionality

--- ftp/ftp_test.go
+++ ftp/ftp_test.go
@@ -1,7 +1,10 @@
 package ftp
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -70,64 +73,77 @@ func TestLogin(t *testing.T) {
 	log.Println("TestLogin completed successfully.")
 }
 
-// func TestWatchDirectory(t *testing.T) {
-// 	log.Println("Running TestWatchDirectory...")
-// 	address, resource := setupFtpServer(t)
-// 	defer teardownFtpServer(t, resource)
+func TestWatchDirectory(t *testing.T) {
+	log.Println("Running TestWatchDirectory...")
+	address, port, resource := setupFtpServer(t)
+	defer teardownFtpServer(t, resource)
+	time.Sleep(10 * time.Second)
+	conf := &ExtraConfig{
+		Username:   "foo",
+		Password:   "pass",
+		Retries:    3,
+		MaxRetries: 3,
+		RemoteDir:  "/home/foo/upload",
+		LocalDir:   "./tmp",
+	}
 
-// 	conf := &ExtraConfig{
-// 		Username:   "foo",
-// 		Password:   "pass",
-// 		Retries:    3,
-// 		MaxRetries: 3,
-// 		RemoteDir:  "/home/foo/upload",
-// 		LocalDir:   "./tmp",
-// 	}
+	log.Printf("Connecting to FTP server at address %s...\n", address)
+	ftpClient, err := Connect(address, port, LocalToRemote, conf)
+	if err != nil {
+		t.Fatalf("Connect returned an error: %v", err)
+	}
 
-// 	log.Printf("Connecting to FTP server at address %s...\n", address)
-// 	ftpClient, err := Connect(address, 21, LocalToRemote, conf)
-// 	if err != nil {
-// 		t.Fatalf("Connect returned an error: %v", err)
-// 	}
+	if ftpClient == nil {
+		t.Fatalf("Connect returned nil FTP")
+	}
 
-// 	if ftpClient == nil {
-// 		t.Fatalf("Connect returned nil FTP")
-// 	}
+	dirToWatch := "./tmp"
+	err = os.MkdirAll(dirToWatch, os.ModePerm)
+	if err != nil {
+		t.Fatalf("Failed to create directory to watch: %v", err)
+	}
+	t.Logf("Created directory to watch: %s\n", dirToWatch)
 
-// 	dirToWatch := "./tmp"
-// 	os.MkdirAll(dirToWatch, os.ModePerm)
-// 	log.Printf("Created directory to watch: %s\n", dirToWatch)
+	go ftpClient.WatchDirectory()
 
-// 	go ftpClient.WatchDirectory()
+	time.Sleep(20 * time.Second)
 
-// 	time.Sleep(20 * time.Second)
+	fileName := "test.txt"
+	filePath := filepath.Join(dirToWatch, fileName)
 
-// 	fileName := "test.txt"
-// 	filePath := filepath.Join(dirToWatch, fileName)
+	t.Logf("Creating file: %s\n", filePath)
+	file, err := os.Create(filePath)
+	if err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+	_, err = file.WriteString("test")
+	if err != nil {
+		t.Fatalf("Failed to write to file: %v", err)
+	}
+	err = file.Close()
+	if err != nil {
+		t.Fatalf("Failed to close file: %v", err)
+	}
+	t.Log("File created and closed.")
 
-// 	log.Printf("Creating file: %s\n", filePath)
-// 	file, err := os.Create(filePath)
-// 	if err != nil {
-// 		t.Fatalf("Failed to create file: %v", err)
-// 	}
-// 	file.WriteString("test")
-// 	file.Close()
-// 	log.Println("File created and closed.")
+	time.Sleep(1 * time.Second)
 
-// 	time.Sleep(1 * time.Second)
+	fmt.Println("Checking remote file status...")
+	remoteFilePath := filepath.Join(conf.RemoteDir, fileName)
+	log.Printf("Checking remote file status for: %s\n", remoteFilePath)
+	_, err = ftpClient.Stat(remoteFilePath)
+	if err != nil {
+		t.Fatalf("Failed to stat file: %v", err)
+	}
 
-// 	remoteFilePath := filepath.Join(conf.RemoteDir, fileName)
-// 	log.Printf("Checking remote file status for: %s\n", remoteFilePath)
-// 	_, err = ftpClient.Stat(remoteFilePath)
-// 	if err != nil {
-// 		t.Fatalf("Failed to stat file: %v", err)
-// 	}
+	log.Printf("Removing directory: %s\n", dirToWatch)
+	err = os.RemoveAll(dirToWatch)
+	if err != nil {
+		t.Fatalf("Failed to remove directory: %v", err)
+	}
+	log.Println("Directory removed.")
 
-// 	log.Printf("Removing directory: %s\n", dirToWatch)
-// 	os.RemoveAll(dirToWatch)
-// 	log.Println("Directory removed.")
+	time.Sleep(1 * time.Second)
 
-// 	time.Sleep(1 * time.Second)
-
-// 	log.Println("TestWatchDirectory completed successfully.")
-// }
+	log.Println("TestWatchDirectory completed successfully.")
+}
