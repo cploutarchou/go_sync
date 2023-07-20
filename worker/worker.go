cc62db58883384dfb0f91f0f654862ed2b68b656
Christos Ploutarcou <cploutarchou@gmail.com>
2023-07-20 21:26:26 +0300

    restore default worker

--- worker/worker.go
+++ worker/worker.go
@@ -1,48 +1,26 @@
 package worker
 
 import (
-	"log"
 	"sync"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-// Task struct for Pool to operate on
+// Task struct for WorkerPool to operate on
 type Task struct {
 	EventType fsnotify.Op
 	Name      string
-	Func      func() error
 }
 
+// WorkerPool is a pool of workers that run tasks
 type Pool struct {
 	Tasks chan Task
 	WG    sync.WaitGroup
 }
 
-func NewWorkerPool(numWorkers int) *Pool {
-	pool := &Pool{
-		Tasks: make(chan Task),
-	}
-
-	pool.WG.Add(numWorkers)
-	for i := 0; i < numWorkers; i++ {
-		go pool.worker()
-	}
-
-	return pool
-}
-
-func (p *Pool) Submit(taskFunc func() error) error {
-	p.Tasks <- Task{Func: taskFunc}
-	return nil
-}
-
-func (p *Pool) worker() {
-	for task := range p.Tasks {
-		err := task.Func()
-		if err != nil {
-			log.Println("Error executing task:", err)
-		}
-		p.WG.Done()
+// NewWorkerPool constructs a new WorkerPool of a given size
+func NewWorkerPool(size int) *Pool {
+	return &Pool{
+		Tasks: make(chan Task, size),
 	}
 }
