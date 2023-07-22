6984eb848177ed246d0dace9a57ccde46969573c
Christos Ploutarchou <cploutarchou@gmail.com>
2023-07-22 22:31:03 +0300

    rename receiver

--- ftp/worker.go
+++ ftp/worker.go
@@ -1,48 +1,45 @@
 package ftp
 
 import (
-	"fmt"
-
 	"github.com/fsnotify/fsnotify"
 )
 
 // Worker starts a new worker goroutine.
-func (s *FTP) Worker() {
-	for task := range s.Pool.Tasks {
+func (f *FTP) Worker() {
+	for task := range f.Pool.Tasks {
 		switch task.EventType {
 		case fsnotify.Create:
-			switch s.Direction {
+			switch f.Direction {
 			case LocalToRemote:
-				fmt.Println("Uploading file:", task.Name)
-				err := s.uploadFile(task.Name)
+				err := f.uploadFile(task.Name)
 				if err != nil {
 					logger.Println("Error uploading file:", err)
 				}
 			case RemoteToLocal:
-				err := s.downloadFile(task.Name)
+				err := f.downloadFile(task.Name)
 				if err != nil {
 					logger.Println("Error downloading file:", err)
 				}
 			}
 		case fsnotify.Write:
-			err := s.uploadFile(task.Name)
+			err := f.uploadFile(task.Name)
 			if err != nil {
 				logger.Println("Error uploading file:", err)
 			}
 		case fsnotify.Remove:
-			switch s.Direction {
+			switch f.Direction {
 			case LocalToRemote:
-				err := s.removeRemoteFile(task.Name)
+				err := f.removeRemoteFile(task.Name)
 				if err != nil {
 					logger.Println("Error deleting file:", err)
 				}
 			case RemoteToLocal:
-				err := s.removeLocalFile(task.Name)
+				err := f.removeLocalFile(task.Name)
 				if err != nil {
 					logger.Println("Error removing remote file:", err)
 				}
 			}
 		}
-		s.Pool.WG.Done()
+		f.Pool.WG.Done()
 	}
 }
